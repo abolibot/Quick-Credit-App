c5611e392e6e4836472ecd611d6d3ee18478d1ca
Oluwatobi Alaran <alarantobiloba@gmail.com>
2019-05-15 13:21:53 +0100

    bug(user-dashboard)fix broken log repayment link
    [finishes #166017832]

--- UI/components/dashboardViews.js
+++ UI/components/dashboardViews.js
@@ -1,88 +1,74 @@
-const pendingVerification = document.querySelector('.dashboard-pending-verification');
-const unverified = document.querySelector('dashboard-unverified-profile');
-const verifiedPlusLoan = document.querySelector('.dashboard-verified-plus-loan');
-const verifiedMinusLoan = document.querySelector('.dashboard-verified-minus-loan');
-const adminDashboard = document.querySelector('.dashboard-admin');
-const pendingLoanApproval = document.querySelector('.dashboard-verified-plus-pending-loan-approval');
-const rejectedLoan = document.querySelector('.dashboard-verified-plus-rejected-loan');
+const clientDashboard = document.querySelector('#client-dashboard');
+const pendingVerification = document.querySelector('#pending-verification');
+const unverified = document.querySelector('#unverified');
+const verifiedPlusLoan = document.querySelector('#verified-plus-loan');
+const verifiedMinusLoan = document.querySelector('#verified-minus-loan');
+const adminDashboard = document.querySelector('#dashboard-admin');
+const pendingLoanApproval = document.querySelector('#pending-loan-approval');
+const rejectedLoan = document.querySelector('#rejected-loan');
+const logRepaymentBtn = document.querySelector('#dashboard-log-repayment-btn');
+const applyForLoanBtn = document.querySelector('#dashboard-apply-button');
+const updateProfileBtn = document.querySelector('#notification-update-profile');
+const viewLoanBtn = document.querySelector('#notification-view-loan');
+const currentLoanContent = document.querySelector('#current-loan-overview-content');
+const verificationStatusBtn = document.querySelector('#my-details-verification-status-btn');
 
 const user = {
   isAdmin: true,
-  isVerified: false,
-  hasCurrentLoan: false,
-  hasPendingLoanApproval: false,
-  hasLoanApplicationRejected: false,
+  isVerifiedPlusLoan: false,
+  isVerifiedMinusLoan: false,
+  isPendingLoanApproval: false,
   isPendingVerification: false,
   isUnverified: false,
   hasRejectedLoan: false,
 };
 
-if (user.admin === true) {
-  pendingVerification.style.display = 'none';
-  unverified.style.display = 'none';
-  verifiedPlusLoan.style.display = 'none';
-  verifiedMinusLoan.style.display = 'none';
-  adminDashboard.style.display = 'block';
-  pendingLoanApproval.style.display = 'none';
-  rejectedLoan.style.display = 'none';
+if (user.isAdmin === true) {
+  adminDashboard.classList.remove('hide');
 }
 
-if (user.isUnverified === true) {
-  pendingVerification.style.display = 'none';
-  unverified.style.display = 'block';
-  verifiedPlusLoan.style.display = 'none';
-  verifiedMinusLoan.style.display = 'none';
-  adminDashboard.style.display = 'none';
-  pendingLoanApproval.style.display = 'none';
-  rejectedLoan.style.display = 'none';
+if (user.isVerifiedPlusLoan === true) {
+  clientDashboard.classList.remove('hide');
+  verifiedPlusLoan.classList.remove('hide');
+  logRepaymentBtn.classList.remove('hide');
+}
+
+if (user.isVerifiedMinusLoan === true) {
+  clientDashboard.classList.remove('hide');
+  verifiedMinusLoan.classList.remove('hide');
+  applyForLoanBtn.classList.remove('disabled');
+  applyForLoanBtn.disabled = false;
+  currentLoanContent.classList.add('hide');
+}
+
+if (user.isPendingLoanApproval === true) {
+  clientDashboard.classList.remove('hide');
+  pendingLoanApproval.classList.remove('hide');
+  viewLoanBtn.classList.remove('hide');
+  currentLoanContent.classList.add('hide');
 }
 
 if (user.isPendingVerification === true) {
-  pendingVerification.style.display = 'block';
-  unverified.style.display = 'none';
-  verifiedPlusLoan.style.display = 'none';
-  verifiedMinusLoan.style.display = 'none';
-  adminDashboard.style.display = 'none';
-  pendingLoanApproval.style.display = 'none';
-  rejectedLoan.style.display = 'none';
+  clientDashboard.classList.remove('hide');
+  pendingVerification.classList.remove('hide');
+  verificationStatusBtn.innerHTML = 'Pending';
+  verificationStatusBtn.classList.remove('verification-status-btn-verified');
+  verificationStatusBtn.classList.add('verification-status-btn-pending');
+  currentLoanContent.classList.add('hide');
 }
 
-if ((user.isVerified === true) && (user.hasCurrentLoan === true)) {
-  pendingVerification.style.display = 'none';
-  unverified.style.display = 'none';
-  verifiedPlusLoan.style.display = 'block';
-  verifiedMinusLoan.style.display = 'none';
-  adminDashboard.style.display = 'none';
-  pendingLoanApproval.style.display = 'none';
-  rejectedLoan.style.display = 'none';
-}
-
-if ((user.isVerified === true) && (user.hasCurrentLoan === false)) {
-  pendingVerification.style.display = 'none';
-  unverified.style.display = 'none';
-  verifiedPlusLoan.style.display = 'none';
-  verifiedMinusLoan.style.display = 'block';
-  adminDashboard.style.display = 'none';
-  pendingLoanApproval.style.display ='none';
-  rejectedLoan.style.display = 'none';
-}
-
-if (user.hasPendingLoanApproval === true) {
-  pendingVerification.style.display = 'none';
-  unverified.style.display = 'none';
-  verifiedPlusLoan.style.display = 'none';
-  verifiedMinusLoan.style.display = 'none';
-  adminDashboard.style.display = 'none';
-  pendingLoanApproval.style.display = 'block';
-  rejectedLoan.style.display = 'none';
+if (user.isUnverified === true) {
+  clientDashboard.classList.remove('hide');
+  unverified.classList.remove('hide');
+  updateProfileBtn.classList.remove('hide');
+  currentLoanContent.classList.add('hide');
+  verificationStatusBtn.classList.remove('verification-status-btn-verified');
+  verificationStatusBtn.classList.add('verification-status-btn-unverified');
+  verificationStatusBtn.innerHTML = 'Unverified';
 }
 
 if (user.hasRejectedLoan === true) {
-  pendingVerification.style.display = 'none';
-  unverified.style.display = 'none';
-  verifiedPlusLoan.style.display = 'none';
-  verifiedMinusLoan.style.display = 'none';
-  adminDashboard.style.display = 'none';
-  pendingLoanApproval.style.display = 'none';
-  rejectedLoan.style.display = 'true';
+  clientDashboard.classList.remove('hide');
+  rejectedLoan.classList.remove('hide');
+  currentLoanContent.classList.add('hide');
 }
