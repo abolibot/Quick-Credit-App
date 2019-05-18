ded939858ee1c52b61028f89c63994dcb22a98fd
Oluwatobi Alaran <alarantobiloba@gmail.com>
2019-05-18 11:25:37 +0100

    chore(sign-in)setup unit tests for sign-in endpoint
    [finishes #165959017]

---
+++ test/test-sign-in.js
@@ -0,0 +1,60 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('signin', () => {
+  it('should create a user and return the user on /api/v1/auth POST', (done) => {
+    chai.request('http://localhost:4000')
+      .post('/api/v1/auth/signin')
+      .send({ 'email': 'alarantobiloba@gmail.com', 'password': 'password1' })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('data');
+        res.body.should.have.property('status');
+        res.body.data.should.be.a('object');
+        res.body.data.should.have.property('email');
+        res.body.data.should.have.property('token');
+        res.body.status.should.equal(200);
+        res.body.data.email.should.equal('alarantobiloba@gmail.com');
+        res.body.data.password.should.equal('password1');
+        done();
+      });
+  });
+
+  it('should return error if email does not exists on /api/v1/auth POST', (done) => {
+    chai.request('http://localhost:4000')
+      .post('/api/v1/auth/signin')
+      .send({ 'email': 'alarantobiloba@gmail.com', 'password': 'password1' })
+      .end((err, res) => {
+        res.should.have.status(401);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('status');
+        res.body.should.have.property('error');
+        res.body.status.should.equal(401);
+        res.body.error.should.equal('invalid login details');
+        done();
+      });
+  });
+
+  it('should return error if password does not match on /api/v1/auth POST', (done) => {
+    chai.request('http://localhost:4000')
+      .post('/api/v1/auth/signin')
+      .send({ 'email': 'alarantobiloba@gmail.com', 'password': 'password1' })
+      .end((err, res) => {
+        res.should.have.status(401);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('status');
+        res.body.should.have.property('error');
+        res.body.status.should.equal(401);
+        res.body.error.should.equal('invalid login details');
+        done();
+      });
+  });
+});
